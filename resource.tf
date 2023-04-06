5f228b7a4b4450e2bacdb8d4972e3b1a76d38e5d
sandeepajay1 <119504658+sandeepajay1@users.noreply.github.com>
2023-04-07 00:48:05 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,40 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "k8s_cluster" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.medium"
+  subnet_id     = "subnet-05a31b67b18113593"
+  vpc_security_group_ids = [
+    "sg-09947aa36d4d8ebb2"
+  ]
 
   tags = {
-    Name = "K8s cluster"
-  }
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.example.id
+    Name = "k8s_cluster"
   }
 }
 
-resource "aws_network_interface" "example" {
-  subnet_id       = "subnet-05a31b67b18113593"
-  security_groups = ["sg-09947aa36d4d8ebb2"]
-  depends_on      = [aws_internet_gateway.gw]
-  attachment {
-    instance = aws_instance.example.id
-  }
-}
-
-resource "aws_eip" "example" {
-  vpc      = true
-  public_ip = "44.213.206.125"
-  depends_on = [aws_internet_gateway.gw]
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "vpc-04015f3645142a1fa"
-}
-
-resource "aws_route_table_association" "example" {
-  subnet_id      = "subnet-05a31b67b18113593"
-  route_table_id = "rtb-0c98e4ab437db89e0"
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.k8s_cluster.id
+  allocation_id = "44.213.206.125"
 }
