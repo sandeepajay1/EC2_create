03ff9946ae6a1f7e8d0653c2bb9ce10a0a8d1412
sandeepajay1 <119504658+sandeepajay1@users.noreply.github.com>
2023-04-07 01:04:38 +0530

    Terraform_file

--- resource.tf
+++ resource.tf
@@ -1,21 +1,56 @@
-provider "aws" {
-  region = "us-east-1"
+# Define variables
+variable "aws_region" {
+  default = "us-east-1"
 }
 
-resource "aws_instance" "k8s_cluster" {
-  ami           = "ami-007855ac798b5175e"
-  instance_type = "t2.medium"
-  subnet_id     = "subnet-05a31b67b18113593"
-  vpc_security_group_ids = [
-    "sg-09947aa36d4d8ebb2"
-  ]
+variable "instance_name" {
+  default = "testServer"
+}
 
+variable "key_name" {
+  default = "Udemy"
+}
+
+variable "security_group_id" {
+  default = "sg-09947aa36d4d8ebb2"
+}
+
+variable "subnet_id" {
+  default = "subnet-05a31b67b18113593"
+}
+
+variable "instance_type" {
+  default = "t2.medium"
+}
+
+variable "ami_id" {
+  default = "ami-007855ac798b5175e"
+}
+
+variable "public_ip" {
+  default = "44.213.206.125"
+}
+
+# Define EC2 instance resource
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [var.security_group_id]
+  subnet_id     = var.subnet_id
   tags = {
-    Name = "k8s_cluster"
+    Name = var.instance_name
   }
 }
 
+# Define Elastic IP resource
+resource "aws_eip" "eip" {
+  vpc      = true
+  depends_on = [aws_instance.ec2_instance]
+}
+
+# Associate Elastic IP with EC2 instance
 resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.k8s_cluster.id
-  allocation_id = "44.213.206.125"
+  instance_id   = aws_instance.ec2_instance.id
+  allocation_id = aws_eip.eip.id
 }
