b53aaaaab76472823c4d722ead9738a3dcff66f4
sandeepajay1 <119504658+sandeepajay1@users.noreply.github.com>
2023-04-07 00:41:53 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,74 +1,41 @@
-# Define variables
-variable "aws_region" {
-  default = "us-east-1"
-}
-variable "instance_name" {
-  default = "testServer"
+provider "aws" {
+  region = "us-east-1"
 }
 
-variable "key_name" {
-  default = "Udemy"
-}
+resource "aws_instance" "example" {
+  ami           = "ami-007855ac798b5175e"
+  instance_type = "t2.medium"
 
-variable "security_group_id" {
-  default = "sg-09947aa36d4d8ebb2"
-}
-variable "vpc_id" {
-  default = "subnet-05a31b67b18113593"
-}
-variable "instance_type" {
-  default = "t2.medium"
-}
-variable "ami_id" {
-  default = "ami-007855ac798b5175e"
-}
-
-variable "public_ip" {
-  default = "44.213.206.125"
-}
-
-# Define Elastic IP resource
-resource "aws_eip" "eip" {
-  vpc      = true
-  depends_on = [aws_internet_gateway.gw]
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Define EC2 instance resource
-resource "aws_instance" "ec2_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  security_groups = [var.security_group_id]
-  subnet_id     = "${var.vpc_id}"
-  associate_public_ip_address = true
   tags = {
-    Name = var.instance_name
+    Name = "K8s cluster"
   }
 
-  # Attach Elastic IP to the instance
   network_interface {
-    device_index = 0
-
-    # Use the Elastic IP created above
-    network_interface_id = aws_network_interface.primary_interface.id
+    device_index         = 0
+    network_interface_id = aws_network_interface.example.id
   }
 }
 
-# Attach Elastic IP to the network interface
-resource "aws_network_interface" "primary_interface" {
-  subnet_id       = var.vpc_id
-  security_groups = [var.security_group_id]
-
-  # Assign the Elastic IP to this network interface
-  associate_public_ip_address = true
-  depends_on = [aws_eip.eip]
-  lifecycle {
-    create_before_destroy = true
+resource "aws_network_interface" "example" {
+  subnet_id       = "subnet-05a31b67b18113593"
+  security_groups = ["sg-09947aa36d4d8ebb2"]
+  depends_on      = [aws_internet_gateway.gw]
+  attachment {
+    instance = aws_instance.example.id
   }
+}
 
-  # Use the Elastic IP created above
-  private_ips = [aws_eip.eip.public_ip]
+resource "aws_eip" "example" {
+  vpc      = true
+  public_ip = "44.213.206.125"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "vpc-04015f3645142a1fa"
+}
+
+resource "aws_route_table_association" "example" {
+  subnet_id      = "subnet-05a31b67b18113593"
+  route_table_id = "rtb-0c98e4ab437db89e0"
 }
