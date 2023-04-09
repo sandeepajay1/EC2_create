8a9c9baba1177e1fd89e99838e8f9416c2fe2483
sandeepajay1 <119504658+sandeepajay1@users.noreply.github.com>
2023-04-09 17:55:28 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,55 +2,32 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-
 variable "instance_name" {
-  default = "testServer"
+  default = "deploy to test"
 }
-
 variable "key_name" {
   default = "Udemy"
 }
-
 variable "security_group_id" {
   default = "sg-09947aa36d4d8ebb2"
 }
-
-variable "subnet_id" {
+variable "vpc_id" {
   default = "subnet-05a31b67b18113593"
 }
-
 variable "instance_type" {
-  default = "t2.medium"
+  default = "t2.micro"
 }
-
 variable "ami_id" {
   default = "ami-007855ac798b5175e"
 }
-
-variable "public_ip" {
-  default = "44.213.206.125"
-}
-
 # Define EC2 instance resource
 resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
   security_groups = [var.security_group_id]
-  subnet_id     = var.subnet_id
+  subnet_id     = "${var.vpc_id}"
   tags = {
     Name = var.instance_name
   }
-}
-
-# Define Elastic IP resource
-resource "aws_eip" "eip" {
-  vpc      = true
-  depends_on = [aws_instance.ec2_instance]
-}
-
-# Associate Elastic IP with EC2 instance
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.ec2_instance.id
-  allocation_id = aws_eip.eip.id
 }
